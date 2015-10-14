a37992c992c72f4067597fd064f3d0942702be85
rektide <rektide@voodoowarez.com>
2015-10-14 15:05:27 -0400

    Initial webpush-pump server.

--- tele-muc-pump.js
+++ tele-muc-pump.js
@@ -1,76 +1,51 @@
-var 
-  fs= require("fs"),
-  path= require("path"),
-  http2 = require("http2"),
-  telestreamitize = require("telestreamitize")
+#!/usr/bin/env node
+var
+  assert= require("assert"),
+  Telestreamitize = require("telestreamitize")
+var
+  Context=    require( "webpush-pump/Context"),
+  Push=       require( "webpush-pump/push/Push"),
+  RouteMaker= require( "webpush-pump/util/route-maker"),
+  Server=     require( "webpush-pump/server"),
+  subscribe=  require( "webpush-pump/subscribe/subscribe"),
+  Subscribe=  require( "webpush-pump/subscribe/Subscribe")
 
-// The callback to handle requests
-function createTeleMucPumpHandler(opts){
-	function teleMucHandler(request, response, cb){
-		if(request.url == '/messages.stream'){
-			// guard 
-			if(teleMucHandler.guard && !response.push){
-				response.writeHead(400)
-				response.end()
-				return
-			}
-			function pushMsg(msg){
-				var push= response.push('/' + msg.sender + '/' + msg.time)
-				push.writeHead(200)
-				push.end(JSON.stringify(msg))
-			}
-			teleMucHandler.telestream.on('*', pushMsg)
-			// TODO: cleanup listener when connection closes
-			if(teleMucHandler.close){
-				response.writeHead(200)
-				response.end()
-			}else{
-				// we're in it for real with a loonnnggg poll
-				// l
-				// o
-				// n
-				// g
-				// p
-				// o
-				// l
-				// l
+// create a server
+var
+  ctx= new Context(),
+  router= RouteMaker( subscribe)( ctx)
+  opts= {
+	ctx: ctx,
+	routes: router.routes(),
+	port: 8090
+  },
+  server= Server( opts)
+Push.warn= true
+Push.okEmpty= true
+console.log( "server created")
 
-				// is
+// make a Subscribeable for "messages"
+var
+  messages= new Subscribe()
+messages.id= "messages"
+ctx.accept( messages)
+console.log( "subscribeable 'messages' created")
 
-				// l
-				// o
-				// n
-				// g
-				// &verticalTab;
-			}
-		}else if(cb){
-			cb()
-		}
-	}
-	teleMucHandler.telestream= opts&& opts.telestream|| new telestreamitize()
-	teleMucHandler.guard= opts&& opts.guard|| null
-	teleMucHandler.close= opts&& opts.close|| null
-}
-
-module.exports= createTeleMucPumpHandler
-
-module.exports.main= function createServer(telestream){
-	// Creating the server in plain or TLS mode (TLS mode is the default)
-	var server
-	if (process.env.HTTP2_PLAIN) {
-		server = http2.raw.createServer({
-			//log: log
-		}, module.exports())
-	} else {
-		server = http2.createServer({
-			//log: log,
-			key: fs.readFileSync(path.join(__dirname, "/localhost.key")),
-			cert: fs.readFileSync(path.join(__dirname, "/localhost.crt"))
-		}, module.exports())
-	}
-	server.listen(process.env.PORT || 8080)
-}
-
-if(module === require.main){
-	module.exports.main()
-}
+// collect & stream irc messages to "messages"
+var
+  room= "#node-dc.dev"
+Telestreamitize({
+	channelFilter: room
+}).then(function( telestream){
+	console.log( "listening for irc messages in '"+ room+ "'")
+	telestream.on( "message", function( msg){
+		var
+		  push= new Push({
+			subscribe: messages
+		  }),
+		  headers= {
+			"Location": ["messages", msg.room.substring(1), msg.time].join( "/")
+		  }
+		push.send( JSON.stringify( msg), headers, ctx)
+	})
+})
